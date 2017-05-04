01735623cec408b926104716842147965b6dbb29
Zero <wjqzero@gmail.com>
2017-05-04 14:57:20 +0800

    Add auto pause OK Add rate nOK

--- app/src/main/java/wjqzero/uic/audiomixer/MainActivity.java
+++ app/src/main/java/wjqzero/uic/audiomixer/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button bc = (Button)findViewById(R.id.btn_test);
         Button bt = (Button)findViewById(R.id.btn_test2);
+        TextView tv = (TextView)findViewById(R.id.tv_rate);
+        SeekBar sb = (SeekBar)findViewById(R.id.sb_rate);
+
+        final int rate;
 
         final SoundPool soundPool;
         soundPool= new SoundPool(10, AudioManager.STREAM_SYSTEM,5);
-        soundPool.load(this,R.raw.a0,1);
+        soundPool.load(this,R.raw.e4,1);
 
         bc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,8 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
         bt.setOnTouchListener(new View.OnTouchListener(){
             public boolean onTouch(View v, MotionEvent event){
+                //rate =
                 if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                    soundPool.play(1, 1, 1, 0, -1, 1);
+                    soundPool.play(1, 1, 1, 0, -1, 0.5F);
+                }
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    soundPool.autoPause();
                 }
                 return true;
             }
